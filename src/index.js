b5bbfb0cb56069e5aa236dda3ab35505c0c4c2e4
mike zhou <66848304+alinh87@users.noreply.github.com>
2020-09-29 21:54:26 +1300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './githubUser.css'
+import App from './components/app'
+
+
+ReactDOM.render(
+    <App />,document.getElementById('root')
+)
